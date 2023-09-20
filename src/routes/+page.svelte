fbaf6ef5f239af44408ae7f8317860a9d841dc6c
Celio Grand <celiogrand@outlook.com>
2023-09-20 12:06:53 +0800

    Init Tailwind CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">
+    Welcome to SvelteKit
+</h1>
+
+<p class="text-xl font-light">
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
